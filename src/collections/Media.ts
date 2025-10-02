3b1b59b7d7c3688e459291760157ccff235e1045
mykeld74 <mike@msdwebsolutions.com>
2025-10-02 14:26:14 -0600

    remove multiple size upload

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -13,27 +13,7 @@ export const Media: CollectionConfig = {
     },
   ],
   upload: {
-    imageSizes: [
-      {
-        name: 'thumbnail',
-        width: 300,
-        height: 400,
-        position: 'centre',
-      },
-      {
-        name: 'card',
-        width: 768,
-        height: 1024,
-        position: 'centre',
-      },
-      {
-        name: 'tablet',
-        width: 1024,
-        height: undefined,
-        position: 'centre',
-      },
-    ],
-    adminThumbnail: 'thumbnail',
+    focalPoint: true,
     mimeTypes: ['image/*'],
   },
 }
